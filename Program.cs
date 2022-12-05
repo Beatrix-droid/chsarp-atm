16d374361166c92db8d0544db45b017cb50e14ba
Beatrice <beatrice.federici123@gmail.com>
2022-12-05 16:36:45 +0000

    simple atm in c#

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 namespace dotnetcore;
-
+using System.Linq;
 
 public class Atm{
     public string name;
     public string surname;
     public double balance;
-    private int PIN;
+    public int PIN;
     
     public Atm(string name, string surname, double balance, int PIN){
         //initilaises user
@@ -94,21 +94,82 @@ public class Atm{
         {
              //create the list of users
             List<Atm> clients = new List<Atm>();
+
             Atm user_1 = new Atm("John", "DOE", 3200, 1234);
             Atm user_2 = new Atm("Joe", "smith", 400.50, 16634);
 
             Atm user_3 = new Atm("Pat", "rick", 33.75, 329);
-        
+    
             clients.Add(user_1);
             clients.Add(user_2);
+            clients.Add(user_3);
+
 
             //here create any number of users
-            Console.WriteLine("Welcome to this atm machine. Please enter your card Pin number");
-            Console.ReadLine();
+            Console.WriteLine("Welcome to this atm machine. Please enter your firstname");
 
-    
+            string first_name = Console.ReadLine();
 
+            Console.WriteLine("Please enter your surname:");
+
+            string last_name = Console.ReadLine();
+
+            bool UserExists = clients.Any(x=> x.name==first_name && x.surname== last_name); 
+
+            if (UserExists){
+                Console.WriteLine("User has been found! Please enter your PIN  number to perform an operation with this atm");
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (UserExists = clients.Any(x=> x.name==first_name && x.surname== last_name && x.PIN==number)){
+                        
+                        Atm found_user = clients.Find(x=> x.name==first_name && x.surname== last_name && x.PIN==number);
+
+                        bool done = false;
+
+                    
+
+                        Console.WriteLine("PIN was entered correctly. Press 1 to view your balance, 2 to make a deposit, 3 to withdraw, or 4 to change your PIN.");
+
+                        int choice = Convert.ToInt32(Console.ReadLine());
+
+                        switch(choice){
+
+                                case 1:
+                                    Console.WriteLine(found_user.get_balance(number));
+                                    break;
+                        
+                                case 2:
+                                    
+                                    Console.WriteLine("Please enter the amount you wish to deposit:");
+                                    float deposit_money = float.Parse(Console.ReadLine());
+                                    Console.WriteLine(found_user.deposit_money(number, deposit_money));
+                                    break;
+                                case 3:
+                                    
+                                    Console.WriteLine("Please enter the ammount you wish to withdraw:");
+                                    float money = float.Parse(Console.ReadLine());
+                                    Console.WriteLine(found_user.withdraw(number, money));
+                                    break;
+                                
+                                case 4:
+                                    Console.WriteLine("Please enter the new pin:");
+                                    int new_pin = Convert.ToInt32(Console.ReadLine());
+                                    Console.WriteLine(found_user.change_Pin(number, new_pin));
+                                    break;
+                                
+                                default:
+                                    Console.WriteLine("number entered is out of range. Press 1 to view your balance, 2 to make a deposit, 3 to withdraw, or 4 to change your PIN. ");
+                                    break;
+                        }
+                }
+                else{
+                    Console.WriteLine("Pin entered incorrecty. Unable to continue with the operation");
+                }
+            }
+            else{
+                Console.WriteLine("Sorry, your credentials were not found in the database. Please make sure you entered the data correctly.");
+            }
             
         }
+        
     }
 
